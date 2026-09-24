2f5b3f8f3a519d43e5602d9cc995afb0c19211cf
D. Lezama <diavalez@gmail.com>
2026-09-24 18:33:19 -0500

    se agregaron métodos a la clase Program

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,11 @@ namespace Promedio1
     {
         static void Main(string[] args)
         {
+            Player player;
+            List<Enemy> enemies = { EnemyOne e1, EnemyTwo e2, EnemyOne e3 };
+            Boss boss;
+            Game g = new Game();
+            g.RunGame(player,enemies);
         }
     }
 }
